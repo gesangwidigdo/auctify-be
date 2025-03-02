e6645c24385c1f109aaa541cd3fe6e744e277449
Gesang Widigdo <gesangwidigdo@gmail.com>
2025-03-02 13:49:35 +0700

    docs: create offer api docs

--- controller/offer.go
+++ controller/offer.go
@@ -18,6 +18,16 @@ func NewOfferController(offerService interfaces.OfferService) interfaces.OfferCo
 }
 
 // Create implements interfaces.OfferController.
+// @Summary Create Offer
+// @Description Create offer data
+// @Tags offer
+// @Accept json
+// @Produce json
+// @Param request body dto.OfferCreateRequest true "Offer Create Request"
+// @Success 201 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Router /offer [post]
 func (o *offerController) Create(ctx *gin.Context) {
 	id, statCode, err := utils.ExtractID(ctx)
 	if err != nil {
@@ -41,6 +51,15 @@ func (o *offerController) Create(ctx *gin.Context) {
 }
 
 // List implements interfaces.OfferController.
+// @Summary Offer List By Auction
+// @Description Get list offer data by auction id
+// @Tags offer
+// @Accept json
+// @Produce json
+// @Param id path string true "Auction ID"
+// @Success 201 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /offer/{id} [get]
 func (o *offerController) List(ctx *gin.Context) {
 	auctionID := ctx.Param("auction_id")
 	uintAuctionID, err := strconv.ParseUint(auctionID, 10, 64)
