4b8f56dc7fe4a62b025357bebabe432c226b217a
Gesang Widigdo <gesangwidigdo@gmail.com>
2025-03-16 15:46:24 +0700

    _

--- config/loadEnv.go
+++ config/loadEnv.go
@@ -8,6 +8,6 @@ import (
 
 func LoadEnv() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file: " + err.Error())
+		log.Print("Error loading .env file: " + err.Error())
 	}
 }
