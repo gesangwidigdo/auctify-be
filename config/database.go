74495cc3e62ca13b41cd7bfe028bcfd5c9df99c9
Gesang Widigdo <gesangwidigdo@gmail.com>
2025-03-16 15:25:05 +0700

    feat: add log and update compose

--- config/database.go
+++ config/database.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"gorm.io/driver/mysql"
@@ -8,12 +9,18 @@ import (
 )
 
 func ConnectDB() *gorm.DB {
-	var db *gorm.DB
 	dsn := os.Getenv("DB_URL")
-	var err error
-	if db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{}); err != nil {
-		panic("Failed to connect to database")
+	if dsn == "" {
+		panic("Environment variable DB_URL not found!")
 	}
 
+	fmt.Println("Connecting to DB with DSN:", dsn)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) 
+	if err != nil {
+		panic("Failed to connect to database: " + err.Error())
+	}
+
+	fmt.Println("Database connected successfully!")
 	return db
 }
